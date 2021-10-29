facd789de173a269a1a1e9b53c89cb363682390c
Almaz <93367141+zamlabek@users.noreply.github.com>
2021-10-29 12:57:04 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
 
 <h2>HTML Imagedfvdv123123321</h2>
 <h3>uluk <h3>
-<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dhulk&psig=AOvVaw3hbZe8VdMVhCHswKR-RgkJ&ust=1635576744838000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCKjSp6mE7_MCFQAAAAAdAAAAABAD" alt="Hulk" width="500" height="333">
+<img src="Trulli" alt="Hulk" width="500" height="333">
 g
 </body>
 </html>
+
 
