e683cd9c4a6b8d121a65eb0d85762b1b0cb609e4
Almaz <93367141+zamlabek@users.noreply.github.com>
2021-11-01 13:56:58 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <body>
 
 
-<h2>HTML Imagedfvdv123123321</h2>
-<h3>uluk <h3>
+<h2>404 not found </h2>
+<h3>sorry<h3>
 <img src="Trulli" alt="Hulk" width="500" height="333">
-g
+
 </body>
 </html>
 
